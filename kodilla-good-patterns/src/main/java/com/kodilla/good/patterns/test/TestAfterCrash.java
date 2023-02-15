9e8330ec0a36ee406a4dc78789c791e86206c247
Karol Popinski <k.popinski@gmail.com>
2023-02-15 17:37:43 +0100

    15.1 exercise

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/test/TestAfterCrash.java
+++
@@ -1,4 +0,0 @@
-package com.kodilla.good.patterns.test;
-
-public class TestAfterCrash {
-}
---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/PatternsRunner.java
@@ -0,0 +1,8 @@
+package com.kodilla.patterns;
+
+public class PatternsRunner {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the nodule 11 - Patterns");
+    }
+}
