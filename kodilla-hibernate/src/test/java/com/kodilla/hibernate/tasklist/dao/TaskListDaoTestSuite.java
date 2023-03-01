cf0550e3882da044bcafeb91812b08c63a14ae01
Karol Popinski <k.popinski@gmail.com>
2023-03-01 19:29:48 +0100

    Task 17.2: Tworzymy encję list zadań

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,36 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+    private static String DESCRIPTION = "Test: Task 17.2 ha ha ha";
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList("ShopList", DESCRIPTION);
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> readTaskList = taskListDao.findByListName(listName);
+        //Then
+        assertEquals(1, readTaskList.size());
+
+        //CleanUp
+        int id = readTaskList.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+}
